c043318f459fdc4d73eb3c655b9f9cb602847f57
Hao On <vanhao2818@gmail.com>
2023-07-09 00:27:30 -0700

    implement subreddit component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import { Toaster } from '@/components/ui/Toaster'
+import Providers from '@/components/Providers'
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
@@ -23,13 +24,16 @@ export default function RootLayout({
       inter.className
     )}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-        {/* @ts-expect-error server component */}
-          <Navbar />
-          { authModal }
-          <div className='container max-w-7xl mx-auto h-full pt-12'>
-            {children}
-          </div>
+          <Providers>
+            {/* @ts-expect-error server component */}
+            <Navbar />
+            { authModal }
+            <div className='container max-w-7xl mx-auto h-full pt-12'>
+              {children}
+            </div>
+          </Providers>
           <Toaster />
+          
       </body>
     </html>
   )
